7583b63268df0df352e1c518fe0c3bb41be24e71
Kirill Klimuk <kklimuk@gmail.com>
2021-08-19 07:27:05 -0700

    head fix

--- src/Head.js
+++ src/Head.js
@@ -1,5 +1,3 @@
-import { isEqual } from 'lodash';
-
 const Head = ({ schema }) => {
   const { metadata = {} } = schema;
   const { React } = Head;
@@ -22,59 +20,95 @@ const Head = ({ schema }) => {
 const useHead = ({ schema, contentWindow }) => {
   const { React } = Head;
 
-  const { metadata } = schema;
-  const [currentMetadata, setCurrentMetadata] = React.useState({});
+  const head = React.useMemo(() => {
+    let element = contentWindow.document.querySelector('head');
+    if (!element) {
+      element = contentWindow.document.createElement('head');
+      document.prepend(element);
+    }
+    return element;
+  }, []);
+
+  const [_metadataState, setMetadataState] = React.useState({
+    current: {},
+    previous: null,
+  });
 
   React.useEffect(() => {
-    if (isEqual(currentMetadata, metadata)) return;
+    setMetadataState((state) => {
+      if (state && useHead.isEqual(schema.metadata, state.current)) return state;
 
-    let head = contentWindow.document.querySelector('head');
-    if (!head) {
-      head = contentWindow.document.createElement('head');
-      document.prepend(head);
-    }
+      const newState = {
+        current: { ...schema.metadata },
+        previous: state.current,
+      };
 
-    const elements = Object.entries(metadata).map(([attribute, value]) => {
-      let element;
-      if (attribute === 'title') {
-        element = head.querySelector('title');
-        element ||= contentWindow.document.createElement('title');
-        if (element.innerText !== value) element.innerText = value;
-      } else {
-        element = head.querySelector(`meta[name="${attribute}"]`);
-        element ||= contentWindow.document.createElement('meta');
-        element.name ||= attribute;
-        if (element.content !== value) element.content = value;
-      }
-      return element;
+      useHead.updateHead(head, newState);
+
+      return newState;
     });
+  }, [schema.metadata]);
 
-    elements.forEach((element) => {
-      if (head.contains(element)) return;
-      head.appendChild(element);
-    });
-    setCurrentMetadata((previous) => {
-      const previousAttributes = Object.keys(previous);
-      const currentAttributes = new Set(Object.keys(metadata));
-      const removedAttributes = previousAttributes.filter((attribute) => (
-        !currentAttributes.has(attribute)
-      ));
-
-      removedAttributes.forEach((attribute) => {
-        let element;
-        if (attribute === 'title') {
-          element = head.querySelector('title');
-        } else {
-          element = head.querySelector(`meta[name="${attribute}"]`);
-        }
-        element?.remove();
+  React.useEffect(() => {
+    const observer = new MutationObserver(() => {
+      setMetadataState((state) => {
+        useHead.updateHead(head, state);
+        return state;
       });
-
-      return metadata;
     });
-  }, [metadata, currentMetadata]);
+    observer.observe(head, { subtree: true, childList: true });
+  }, []);
 
-  return metadata;
+  return schema.metadata;
+};
+
+useHead.updateHead = (head, { current: metadata, previous }) => {
+  const document = head.ownerDocument;
+  const elements = Object.entries(metadata).map(([attribute, value]) => {
+    let element;
+    if (attribute === 'title') {
+      element = head.querySelector('title');
+      element ||= document.createElement('title');
+      if (element.innerText !== value) element.innerText = value;
+    } else {
+      element = head.querySelector(`meta[name="${attribute}"]`);
+      element ||= document.createElement('meta');
+      element.name ||= attribute;
+      if (element.content !== value) element.content = value;
+    }
+    return element;
+  });
+
+  elements.forEach((element) => {
+    if (head.contains(element)) return;
+    head.appendChild(element);
+  });
+
+  const previousAttributes = Object.keys(previous);
+  const currentAttributes = new Set(Object.keys(metadata));
+  const removedAttributes = previousAttributes.filter((attribute) => (
+    !currentAttributes.has(attribute)
+  ));
+
+  removedAttributes.forEach((attribute) => {
+    let element;
+    if (attribute === 'title') {
+      element = head.querySelector('title');
+    } else {
+      element = head.querySelector(`meta[name="${attribute}"]`);
+    }
+    element?.remove();
+  });
+};
+
+useHead.isEqual = (previous, current) => {
+  const [previousEntries, currentEntries] = [previous, current].map((metadata) => (
+    Object.entries(metadata)
+  ));
+
+  if (previousEntries.length !== currentEntries.length) return false;
+
+  return !previousEntries.some(([attribute, value]) => current[attribute] !== value);
 };
 
 export default Head;
