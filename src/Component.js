8ee0bc838a9430f7cd19777e9077f49d122c698c
Kirill Klimuk <kklimuk@gmail.com>
2021-08-20 14:48:42 -0700

    more fix

--- src/Component.js
+++ src/Component.js
@@ -53,7 +53,7 @@ Component.useSchema = ({ lookupBy, passedSchema }) => {
   const { schemas: schemaCache } = Component.directory;
   const [isClient, setIsClient] = Component.React.useState(false);
   const computedSchema = Component.React.useMemo(() => {
-    let schema = passedSchema || null;
+    let schema = passedSchema || schemaCache[lookupBy] || null;
     if (isClient) schema = schemaCache[lookupBy] || schema;
     if (!isNullish(schema)) schema = schema?.isSchema ? schema : Schema.parseComponentJSON(schema);
 
